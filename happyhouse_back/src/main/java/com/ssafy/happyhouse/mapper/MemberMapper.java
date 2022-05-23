de7d2249c2901bab78ef5f1f0a670ddd37e9f33d
최진우 <freiya1002@naver.com>
2022-05-24 04:21:55 +0900

    update sts about member

--- happyhouse_back/src/main/java/com/ssafy/happyhouse/mapper/MemberMapper.java
+++ happyhouse_back/src/main/java/com/ssafy/happyhouse/mapper/MemberMapper.java
@@ -10,7 +10,9 @@ public interface MemberMapper {
 	public MemberDto login(MemberDto memberDto) throws SQLException;
 	public MemberDto userInfo(String userid) throws SQLException;
 	public int registerUser(MemberDto memberDto) throws SQLException;
-	public int modifyUser(MemberDto memberDto) throws SQLException;
+	public int updateUser(MemberDto memberDto) throws SQLException;
+	public int updatePassword(MemberDto memberDto) throws SQLException;
 	public int deleteUser(String userid) throws SQLException;
 	public int idCheck(String userid) throws SQLException;
+	public String findPWD(MemberDto memberDto) throws SQLException;
 }
