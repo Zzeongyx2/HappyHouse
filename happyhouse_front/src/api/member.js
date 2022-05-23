e062d6a4db2cd3931b300b3e68cab98e67b7192e
최진우 <freiya1002@naver.com>
2022-05-23 10:57:58 +0900

    first merge 0523 1057

---
+++ happyhouse_front/src/api/member.js
@@ -0,0 +1,16 @@
+import { apiInstance } from "./index.js";
+
+const api = apiInstance();
+
+async function login(user, success, fail) {
+  await api.post(`/user/login`, JSON.stringify(user)).then(success).catch(fail);
+}
+
+async function findById(userid, success, fail) {
+  api.defaults.headers["access-token"] = sessionStorage.getItem("access-token");
+  await api.get(`/user/info/${userid}`).then(success).catch(fail);
+}
+
+// function logout(success, fail)
+
+export { login, findById };
---
+++ happyhouse_front/src/store/modules/houseStore.js
@@ -0,0 +1,114 @@
+import { sidoList, gugunList, dongList, houseList } from "@/api/house.js";
+
+const houseStore = {
+  namespaced: true,
+  state: {
+    sidos: [{ value: null, text: "선택하세요" }],
+    guguns: [{ value: null, text: "선택하세요" }],
+    dongs: [{ value: null, text: "선택하세요" }],
+    houses: [],
+    house: null,
+  },
+
+  getters: {},
+
+  mutations: {
+    SET_SIDO_LIST: (state, sidos) => {
+      sidos.forEach((sido) => {
+        state.sidos.push({ value: sido.sidoCode, text: sido.sidoName });
+      });
+    },
+    SET_GUGUN_LIST: (state, guguns) => {
+      guguns.forEach((gugun) => {
+        state.guguns.push({ value: gugun.gugunCode, text: gugun.gugunName });
+      });
+    },
+    SET_DONG_LIST: (state, dongs) => {
+      dongs.forEach((dong) => {
+        state.dongs.push({ value: dong.dongCode, text: dong.dongName });
+      });
+    },
+    CLEAR_SIDO_LIST: (state) => {
+      state.sidos = [{ value: null, text: "선택하세요" }];
+    },
+    CLEAR_GUGUN_LIST: (state) => {
+      state.guguns = [{ value: null, text: "선택하세요" }];
+    },
+    CLEAR_DONG_LIST: (state) => {
+      state.dongs = [{ value: null, text: "선택하세요" }];
+    },
+    CLEAR_HOUSE_LIST: (state) => {
+      state.houses = [];
+    },
+    SET_HOUSE_LIST: (state, houses) => {
+      //   console.log(houses);
+      state.houses = houses;
+    },
+    SET_DETAIL_HOUSE: (state, house) => {
+      state.house = house;
+    },
+  },
+
+  actions: {
+    getSido: ({ commit }) => {
+      sidoList(
+        ({ data }) => {
+          // console.log(data);
+          commit("SET_SIDO_LIST", data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    getGugun: ({ commit }, sidoCode) => {
+      const params = {
+        sido: sidoCode,
+      };
+      gugunList(
+        params,
+        ({ data }) => {
+          // console.log(commit, response);
+          commit("SET_GUGUN_LIST", data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    getDong: ({ commit }, gugunCode) => {
+      const params = { gugun: gugunCode };
+      dongList(
+        params,
+        ({ data }) => {
+          commit("SET_DONG_LIST", data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    getHouseList: ({ commit }, dongCode) => {
+      const params = {
+        dong: dongCode,
+      };
+      houseList(
+        params,
+        (response) => {
+          //   console.log(response.data.response.body.items.item);
+          commit("SET_HOUSE_LIST", response.data);
+          console.log(response.data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    detailHouse: ({ commit }, house) => {
+      // 나중에 house.일련번호를 이용하여 API 호출
+      commit("SET_DETAIL_HOUSE", house);
+    },
+  },
+};
+
+export default houseStore;
